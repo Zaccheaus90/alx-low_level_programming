00952a429e33c0796a2afe4384087cff70c1999a
root <afolabizacchaeus01@gmail.com>
2022-03-17 06:57:45 -0700

    addin files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+ /**
+  *  main - test function that print if integer is positive or negative
+  *  Return: 0
+  */
+int main(void)
+{
+	 int i;
+	  
+	 i = 0;
+	 postive_or_negative(i);
+
+	 return (0);
+}
