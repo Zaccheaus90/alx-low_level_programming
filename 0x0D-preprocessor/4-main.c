5ea24f7ad06f46b92036ce507e64bd8c0ef9b005
root <afolabizacchaeus01@gmail.com>
2022-04-12 20:42:01 -0700

    add

--- 0x0D-preprocessor/4-main.c
+++ 0x0D-preprocessor/4-main.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include "4-sum.h"
 #include "4-sum.h"
 
@@ -8,10 +8,8 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    int s;
-
-    s = SUM(98, 1024);
-    printf("%d\n", s);
-    return (0);
+{int s;
+s = SUM(98, 1024);
+printf("%d\n", s);
+return (0);
 }
